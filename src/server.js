c1e734eecbadb93d794c5d97a4ead38b7ad4f56e
Alvaro <almartin@fi.uba.ar>
2021-02-05 03:08:21 -0300

    Mejoras de sintaxis

--- src/server.js
+++ src/server.js
@@ -38,48 +38,89 @@ const io = SocketIO(server, {
     }
 });
 
+function registrarConexion(idChat, name, socketId) {
+    const body = {
+        idChat,
+        name,
+        socketId
+    }
+
+    const newUserConnection = new userConnection(body);
+    newUserConnection.save();
+}
+
+async function autenticarConexion(nameUser) {
+    const response = await chatId.find();
+    
+    if (response.length == 0) {
+    
+        const newChatId = new chatId({idChat: 0})
+        await newChatId.save();
+        return {idChat: 0, name: nameUser}
+    
+    } else {
+    
+        const current = response[0].idChat;
+        const dataUpdate = await chatId.findByIdAndUpdate(response[0]._id, {idChat: current + 1});
+        return {idChat: current + 1, name: nameUser}
+    
+    }
+}
+
+async function eliminarConexion(socketId) {
+    const responseFind = await userConnection.find({socketId});
+    const responseDelete = await userConnection.findByIdAndDelete(responseFind[0]._id);
+    return responseDelete._id;
+}
+
+function validarRespuestaTelegram(ctx) {
+    return new Promise((resolve, reject) => {
+        const msj = ctx.update.message.text;
+        if (msj[0] != '@') return reject("Error, falta el caracter '@'");
+    
+        let i = 0;
+        
+        for (i; msj[i] != ":" && i < msj.length; i++);
+        
+        if (i === msj.length) return reject("Error, falta el caracter ':'");
+        if (msj[i + 1] != " ") return reject("Error, falta un espacio entre ':' y tu respuesta.");
+        
+        const idChat = msj.slice(1, i);
+        const msjChat = msj.slice(i + 2);
+        resolve({idChat, msjChat});
+    
+    });
+}
+
 io.on('connection', socket => {
-    socket.on('auth', (nameUser) => {
-        let i = 0
-        chatId.find().exec((err, data) => {
-            if (data.length == 0) {
-                const newChatId = new chatId({idChat: 0})
-                newChatId.save();
-            }
-            else {
-                const current = data[0].idChat;
-                chatId.update({idChat: current}, {$inc: {idChat: current + 1}});
-                i = current + 1;
-            }
-        });
-        userConnection.find({username: nameUser}).exec((err, data) => {
-            const newUserConnection = new userConnection({
-                socketId: socket.id,
-                name: nameUser,
-                idChat: i
-            });
-            newUserConnection.save();
-        });
+    let socketId = socket.id;
+    let idChat = 0;
+    let name;
+
+    socket.on('auth', async (nameUser) => {
+        const data = await autenticarConexion(nameUser);
+        idChat = data.idChat;
+        name = data.name;
+        registrarConexion(idChat, name, socketId);
+    });
+        
+    socket.on('message', (user) => {
+        const msj = "From: " + name + "\n" + "ID: @" + idChat + "\n" + "Message: " + user.text;
+        bot.telegram.sendMessage(855202158, msj);
     });
 
-    socket.on('message', async (user) => {
-        await userConnection.find({socketId: socket.id}).exec((err, data) => {
-            const idChat = data[0].idChat;
-            const msj = "From: " + data[0].name + "\n" + "ID: @" + idChat + "\n" + "Message: " + user.text;
-            bot.telegram.sendMessage(855202158, msj);
-        });
+    socket.on('disconnect', async () => {
+        await eliminarConexion(socketId);
     });
 
     bot.on('text', async (ctx) => {
-        const msj = ctx.update.message.text;
-        let i = 0;
-        for (i; msj[i] != ":"; i++);
-        const idChat = msj.slice(1, i);
+        try {
+            
+            const { idChat, msjChat } = await validarRespuestaTelegram(ctx);
+            const responseFind = await userConnection.find({idChat});
+            io.to(responseFind[0].socketId).emit('message', msjChat);
 
-        await userConnection.find({idChat}).exec((err, data) => {
-            const socketId = data[0].socketId;
-            io.to(socketId).emit('message', msj.slice(i + 2));
-        });
+        } catch (error) { bot.telegram.sendMessage(855202158, error) }
     });
 });
 
